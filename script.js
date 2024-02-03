fe22160c55fccfd85008520c00c12ddd30441e18
DevonTech <devontech13@gmail.com>
2024-02-02 21:28:08 -0500

    Added logic for a single round to test outcomes

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const rockPaperScissors = ['rock', 'paper', 'scissors'];
 let computerChoice;
 let userChoice;
+let winner;
 
 //get computerInput
 const computerInput = () => {
@@ -15,7 +16,8 @@ const computerInput = () => {
         computerChoice = 'scissors';
     };
     return computerChoice;
-}
+};
+
 //Get userInput
 const userInput = () => {
     userChoice = prompt('Choose rock, paper, or scissors').toLowerCase();
@@ -23,11 +25,34 @@ const userInput = () => {
         userInput()
     }
     return userChoice
-}
+};
 
 //compare inputs
+computerInput();
+userInput();
+const singleRound = (computerChoice, userChoice) => {
+    if (userChoice !== computerChoice){
+        if (
+            (userChoice === 'rock' && computerChoice === 'scissors') ||
+            (userChoice === 'paper' && computerChoice === 'rock') ||
+            (userChoice === 'scissors' && computerChoice === 'paper')
+            ) 
+            {
+                winner = 'player';
+        };
+        if (
+            (computerChoice === 'rock' && userChoice === 'scissors') ||
+            (computerChoice === 'paper' && userChoice === 'rock') ||
+            (computerChoice === 'scissors' && userChoice === 'paper')
+            ){
+                winner = 'computer';
+            };
+            return winner;
+    };
+    winner = 'draw';
+    return winner;
+};
 
+singleRound(computerChoice, userChoice);
+console.log(winner);
 //run a game best of five
-userInput()
-computerInput()
-console.log(userChoice, computerChoice);
